d9a39af32b407a258abdd65a11090bfea7beedc0
Jorge Lara Ticona <laraticonajorge@gmail.com>
2016-12-22 00:53:33 -0400

    agregando funcionalidad a el listado de musica de SD card

--- app/src/main/java/com/androidbolivia/studyjamproject/listadoPantalla.java
+++ app/src/main/java/com/androidbolivia/studyjamproject/listadoPantalla.java
@@ -1,8 +1,11 @@
 package com.androidbolivia.studyjamproject;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.io.File;
 
 /**
  * Created by JORGE on 18/12/2016.
@@ -12,11 +15,23 @@ public class listadoPantalla extends FullscreenActivity{
 
     private ListView lvLista;
     private ArrayAdapter<String> adaptador;
-    private String [] datos = {"hola","todos","putos","amos","mejores","iron","maiden","la mejor","banda","del NWBAM","JEJEJEJE"};
+    private String [] datos = new String [500];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.listado_layout);
+        String path = Environment.getExternalStorageDirectory().getPath()+"/Music";
+        String files;
+        File folder = new File(path);
+        File[] listOfFiles = folder.listFiles();
+        for (int i = 0; i < listOfFiles.length; i++) {
+            if (listOfFiles[i].isFile()) {
+                datos[i] = listOfFiles[i].getName().toString();
+                /*if (files.endsWith(".mp3") || files.endsWith(".MP3")) {
+                    Toast.makeText(getApplicationContext(), files, Toast.LENGTH_LONG).show();
+                }*/
+            }
+        }
         lvLista = (ListView) findViewById(R.id.lvLista);
         adaptador = new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1 , datos);
         lvLista.setAdapter(adaptador);
--- app/src/main/java/com/androidbolivia/studyjamproject/segundaPantalla.java
+++ app/src/main/java/com/androidbolivia/studyjamproject/segundaPantalla.java
@@ -1,9 +1,15 @@
 package com.androidbolivia.studyjamproject;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.File;
 
 /**
  * Created by JORGE on 18/12/2016.
@@ -12,11 +18,29 @@ import android.widget.Button;
 public class segundaPantalla extends FullscreenActivity{
 
     private Button btnListado;
+    private Button btnPlay;
+    private int sw = 0;
+    private MediaPlayer mp;
+    private String datos[] = new String[500];
+    private TextView nombre;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.segunda_activity);
         btnListado = (Button) findViewById(R.id.btnListado);
+        btnPlay = (Button) findViewById(R.id.play);
+        nombre = (TextView) findViewById(R.id.tvNombre);
+        String path = Environment.getExternalStorageDirectory().getPath()+"/Music";
+        File folder = new File(path);
+        File[] listOfFiles = folder.listFiles();
+        for (int i = 0; i < listOfFiles.length; i++) {
+            if (listOfFiles[i].isFile()) {
+                datos[i] = listOfFiles[i].getName();
+                /*if (files.endsWith(".mp3") || files.endsWith(".MP3")) {
+                    Toast.makeText(getApplicationContext(), files, Toast.LENGTH_LONG).show();
+                }*/
+            }
+        }
         btnListado.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -25,5 +49,23 @@ public class segundaPantalla extends FullscreenActivity{
                 startActivityForResult(intent, rcod);
             }
         });
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(sw == 0) {
+                    String a = "/Music/" + datos[30];
+                    Uri datos = Uri.parse(Environment.getExternalStorageDirectory().getPath() + a);
+                    mp = MediaPlayer.create(getApplicationContext(), datos);
+                    mp.start();
+                    sw = 1;
+                    nombre.setText(a);
+                }
+                else{
+                    mp.pause();
+                    sw = 0;
+                }
+            }
+        });
     }
+
 }
